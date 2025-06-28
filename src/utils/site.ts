dff2379704194bebf3b6a41a4d883d7e21be072a
eslasojica <keslasojica@gmail.com>
2025-06-29 00:58:14 +0700

    Add files via upload

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,4 @@
 export const nama = 'Pornub';
-export const desk = 'Video bokep indo jepang viral ngentot colmek orgasme bocil smp jilbab gangbang smk.';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Jepang Viral Sma Cindo Tante China';
 export const url = 'https://pornub.pages.dev';
-export const terbit = '2025-06-25T00:00:00Z';
+export const terbit = '2025-06-25T00:01:03Z';
